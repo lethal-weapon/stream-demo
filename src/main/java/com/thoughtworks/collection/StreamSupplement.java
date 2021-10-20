7cb4a9b109a300bc04ba10443695d247c167e1ed
Sean Kramer <sean@kramerdesigns.app>
2021-10-20 11:49:10 +0800

    [Stream][Sean]feature: complete stream misc demo

--- src/main/java/com/thoughtworks/collection/StreamSupplement.java
+++ src/main/java/com/thoughtworks/collection/StreamSupplement.java
@@ -1,24 +1,32 @@
 package com.thoughtworks.collection;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamSupplement {
 
-    public StreamSupplement() {
-    }
+  public StreamSupplement() {
+  }
 
-    public List<Integer> sortFromMaxToMin(List<Integer> numbers) {
-        return null;
-    }
+  public List<Integer> sortFromMaxToMin(List<Integer> numbers) {
+    return numbers
+      .stream()
+      .sorted((n1, n2) -> n2 - n1)
+      .collect(Collectors.toList());
+  }
 
-    public double getAverage(List<Integer> numbers) {
-        return 0;
-    }
+  public double getAverage(List<Integer> numbers) {
+    return numbers
+      .stream()
+      .mapToInt(Integer::new)
+      .average()
+      .orElse(0d);
+  }
 
-    public int getMaxValue(List<Integer> numbers) {
-        return 0;
-    }
-
+  public int getMaxValue(List<Integer> numbers) {
+    return numbers
+      .stream()
+      .max(Integer::compareTo)
+      .orElse(0);
+  }
 }
--- src/test/java/com/thoughtworks/collection/StreamSupplementTest.java
+++ src/test/java/com/thoughtworks/collection/StreamSupplementTest.java
@@ -9,38 +9,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StreamSupplementTest {
 
-    @Test
-    public void should_sort_from_max_to_min() {
-        //order from max to min
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        List<Integer> expectedSortedNumbers = Arrays.asList(5, 4, 3, 2, 1);
+  @Test
+  public void should_sort_from_max_to_min() {
+    //order from max to min
+    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+    List<Integer> expectedSortedNumbers = Arrays.asList(5, 4, 3, 2, 1);
 
-        List<Integer> actualSortedNumbers = new StreamSupplement().sortFromMaxToMin(numbers);
+    List<Integer> actualSortedNumbers = new StreamSupplement().sortFromMaxToMin(numbers);
 
-        assertEquals(expectedSortedNumbers, actualSortedNumbers);
-    }
+    assertEquals(expectedSortedNumbers, actualSortedNumbers);
+  }
 
-    @Test
-    public void should_get_average_of_list() {
-        //get the average value
-        //double numberA = 1.00000001;
-        //double numberB = 0.00000001;
-        List<Integer> numbers = Arrays.asList(12, 34, 56, 78, 90, 21);
-        double expectedAverage = 48.5;
+  @Test
+  public void should_get_average_of_list() {
+    //get the average value
+    //double numberA = 1.00000001;
+    //double numberB = 0.00000001;
+    List<Integer> numbers = Arrays.asList(12, 34, 56, 78, 90, 21);
+    double expectedAverage = 48.5;
 
-        double actualAverage = new StreamSupplement().getAverage(numbers);
+    double actualAverage = new StreamSupplement().getAverage(numbers);
 
-        assertEquals(expectedAverage, actualAverage);
-    }
+    assertEquals(expectedAverage, actualAverage);
+  }
 
-    @Test
-    public void should_return_max_value() {
-        //get the max value
-        List<Integer> numbers = Arrays.asList(1, 11, 27, 20, 4, 9, 15, 4, 1, 11);
-        int expectedMaxValue = 27;
+  @Test
+  public void should_return_max_value() {
+    //get the max value
+    List<Integer> numbers = Arrays.asList(1, 11, 27, 20, 4, 9, 15, 4, 1, 11);
+    int expectedMaxValue = 27;
 
-        int actualMaxValue = new StreamSupplement().getMaxValue(numbers);
+    int actualMaxValue = new StreamSupplement().getMaxValue(numbers);
 
-        assertEquals(expectedMaxValue, actualMaxValue);
-    }
+    assertEquals(expectedMaxValue, actualMaxValue);
+  }
 }
