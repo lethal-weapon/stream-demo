4d3c33233fc715b9c3b8fb39f5aa234b1f80099f
Sean Kramer <sean@kramerdesigns.app>
2021-10-20 11:39:29 +0800

    [Stream][Sean]feature: complete stream reduce demo

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -4,15 +4,26 @@ import java.util.List;
 
 public class StreamReduce {
 
-    public int getLastOdd(List<Integer> numbers) {
-        return 0;
-    }
+  public int getLastOdd(List<Integer> numbers) {
+    return numbers
+      .stream()
+      .filter(n -> n % 2 != 0)
+      .reduce((n1, n2) -> n2)
+      .orElse(0);
+  }
 
-    public String getLongest(List<String> words) {
-        return null;
-    }
+  public String getLongest(List<String> words) {
+    return words
+      .stream()
+      .reduce((w1, w2) -> w1.length() >= w2.length() ? w1 : w2)
+      .orElse("");
+  }
 
-    public int getTotalLength(List<String> words) {
-        return 0;
-    }
+  public int getTotalLength(List<String> words) {
+    return words
+      .stream()
+      .reduce(String::concat)
+      .orElse("")
+      .length();
+  }
 }
